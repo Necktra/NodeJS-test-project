a9f8e11d9878a382693194dbde1142b475b814e7
Irina <hallo.ween@mail.ru>
2022-01-09 22:54:49 +0300

    html page web server

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const server = http.createServer((req, res) => {
     if (req.method === 'GET') {
         res.writeHead(200, {
-            'Content-Type': 'text/html'
-        })
-        res.end(`
-            <h1>Form</h1>
-            <form method="post" action="/">
-                <input name="title" type="text" />
-                <button type="submit">Send</button>
-            </form>
-        `)
+            'Content-Type': 'text/html; charset=utf-8'
+        });
+
+        if (req.url === '/') {
+            fs.readFile(path.join(__dirname, 'views', 'index.html'),
+            'utf-8',
+            (err, content) => {
+                if (err) {throw err};
+                res.end(content);
+            })
+        } else if (req.url === '/about') {
+            fs.readFile(path.join(__dirname, 'views', 'about.html'),
+            'utf-8',
+            (err, content) => {
+                if (err) {throw err};
+                res.end(content);
+            })
+        }
+        
     } else if (req.method === "POST") {
         const body = [];
         res.writeHead(200, {
             'Content-Type': 'text/html; charset=utf-8'
-        })
+        });
 
         req.on('data', data => {
             body.push(Buffer.from(data))
