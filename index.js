b9c6805360d7187945d83cb9641e55b9921d3775
Irina <hallo.ween@mail.ru>
2022-01-09 22:22:05 +0300

    base server test

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
+    if (req.method === 'GET') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        res.end(`
+            <h1>Form</h1>
+            <form method="post" action="/">
+                <input name="title" type="text" />
+                <button type="submit">Send</button>
+            </form>
+        `)
+    } else if (req.method === "POST") {
+        const body = [];
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'
+        })
 
-    res.write('<h1>Hello from NodeJS</h1>');
-    res.write('<h2>Hello from NodeJS!</h2>');
-    res.end('<h1>test</h1>');
-})
+        req.on('data', data => {
+            body.push(Buffer.from(data))
+        });
+
+        req.on('end', () => {
+            const message = body.toString().split('=')[1];
+
+            res.end(`
+           <h1>Ваше сообщение: ${message}</h1>
+           `);
+        });
+    }
+});
 
 server.listen(3000, () => {
     console.log("Server is running")
